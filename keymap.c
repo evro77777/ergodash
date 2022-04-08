21a039d15188ab9711caba2770ee2eec5580b793
evro77777 <90ipatov90@gmail.com>
2022-04-08 14:58:22 +0300

    edit keymap

--- keymap.c
+++ keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | alt  |   A  |   S  |   D  |   F  |   G  |   `  |                    | mute |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | shift|   Z  |   X  |   C  |   V  |   B  |||||||| lgui |      |ALT_TB||||||||   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | !    |   Z  |   X  |   C  |   V  |   B  |||||||| lgui |      |ALT_TB||||||||   N  |   M  |   ,  |   .  |   /  | ?    |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * |      | del  | enter|  lang||||||||Lower| Space| ctrl ||||||| | Enter|shift |BspRse|||||||| W_C_L| W_C_R|  ]   | Rctrl|
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      TD(TD_ESC_GRV),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   XXXXXXX,                       KC_VOLU, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,       XXXXXXX,
      KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_BRK,                        KC_VOLD, KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,       KC_LBRC,
      KC_LALT,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_GRV,                        KC_MUTE, KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,    KC_QUOT,
-    KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_LGUI,     ALT_TAB,  KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
-     XXXXXXX,          XXXXXXX,  XXXXXXX,  LGUI(KC_SPC),   KC_DEL,  LT(1,KC_SPC),  CTL_ENT,     KC_ENT , OSM(MOD_LSFT), LT(2,KC_BSPC),    LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RGHT)), KC_RBRC,    KC_RCTL
+     KC_EXLM,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_LGUI,     ALT_TAB,  KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,    KC_QUES,
+     XXXXXXX,          XXXXXXX,  XXXXXXX,  LGUI(KC_SPC),   LT(1,KC_DEL),  KC_SPC,  CTL_ENT ,     KC_ENT , OSM(MOD_LSFT), LT(2,KC_BSPC),    LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RGHT)), KC_RBRC,    KC_RCTL
   ),
 
 
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * | Reset|      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |rgbtog|bl_tog|      |                    |      |      |      |      |      |      |      |
+   * |      |      |      | cycle|rgbtog|bl_tog|      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Raise|      |rgbhui|rgbsai|rgbvai|bl_inc|      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] = LAYOUT(
       RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, BL_TOGG, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_TOG, BL_TOGG, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       RAISE, XXXXXXX, RGB_HUI, RGB_SAI, RGB_VAI,  BL_INC, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     EEP_RST, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,  BL_DEC,           XXXXXXX,     XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
     XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,             LOWER,   KC_SPC ,XXXXXXX,     XXXXXXX,  KC_SPC,   RAISE,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
