0ecbef84708459a5524b6d7de90f4c9ce4c7c5fd
evro77777 <90ipatov90@gmail.com>
2022-11-05 17:55:11 +0300

    05\11\2022

--- keymap.c
+++ keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | alt  |   A  |   S  |   D  |   F  |   G  | Tab  |                    | mute |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |win   |   Z  |   X  |   C  |   V  |   B  |||||||| alt  |      |tab   ||||||||   N  |   M  |   ,  |   .  |   /  | ?    |
-   * |      |      |      |  lang||||||||Lower| Space | ctrl ||||||| | Enter|shift|BspRse||||||| W_C_L| W_C_R|  ]    | Rctrl|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * |      |      |      |  lang||||||||Lower| Space | ctrl ||||||| | Enter|shift|BspRse||||||| W_C_L| W_C_R |  ]   |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( 
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,           KC_Q,    KC_W,    KC_E,           KC_R,    KC_T,   KC_BRK,                        KC_VOLD,          KC_Y,             KC_U,                KC_I,                KC_O,    KC_P,       KC_LBRC,
      KC_LALT,          KC_A,    KC_S,    KC_D,           KC_F,    KC_G,   KC_TAB,                        KC_MUTE,          KC_H,             KC_J,                KC_K,                KC_L,    KC_SCLN,    KC_QUOT,
      KC_LGUI,          KC_Z,    KC_X,    KC_C,           KC_V,    KC_B,         KC_LALT,           KC_TAB,                 KC_N,             KC_M,                KC_COMM,             KC_DOT,  KC_SLSH,    KC_QUES,
-     XXXXXXX,          XXXXXXX, XXXXXXX, LGUI(KC_SPC),   LOWER,   KC_SPC,       CTL_ENT ,          KC_ENT,OSM(MOD_LSFT),   LT(2,KC_BSPC),    LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RGHT)),          KC_RBRC,    KC_RCTL
+     XXXXXXX,          XXXXXXX, XXXXXXX, LGUI(KC_SPC),   LOWER,   KC_SPC,       CTL_ENT ,          KC_ENT,OSM(MOD_LSFT),   LT(2,KC_BSPC),    LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RGHT)),          KC_RBRC,    XXXXXXX
   ),
 
 
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX,    XXXXXXX, KC_LPRN,            KC_RPRN,          XXXXXXX,    XXXXXXX,                         XXXXXXX,  XXXXXXX,       KC_RCBR,        KC_LCBR,       XXXXXXX,         XXXXXXX,        KC_F12,
       RAISE, KC_1 ,      KC_2,    KC_3,               KC_4,             KC_5,       XXXXXXX,                         XXXXXXX,  KC_6,          KC_7,           KC_8,          KC_9,            KC_0,           XXXXXXX,
     KC_LSFT, XXXXXXX,    XXXXXXX, LCTL(KC_PGUP),      LCTL(KC_PGDOWN),  XXXXXXX,              XXXXXXX,      XXXXXXX,           KC_MINS,       KC_PLUS,        KC_EQUAL,      KC_UNDERSCORE,   KC_BSLS,        KC_RSFT,
-    XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,            LOWER,            KC_SPC ,              KC_ENT,       KC_ENT, KC_RSFT ,  KC_BSPC,       XXXXXXX  ,     XXXXXXX,        XXXXXXX,         XXXXXXX 
+    XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,            LOWER,            KC_SPC ,              KC_ENT,       KC_ENT, KC_RSFT ,  KC_BSPC,                       XXXXXXX  ,     XXXXXXX,        XXXXXXX,         XXXXXXX 
   ),
 
   /* Raise
@@ -90,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,                      XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX,       XXXXXXX,             XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, LCTL(KC_LEFT),             LCTL(KC_RGHT),                XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, KC_HOME,       KC_PGUP,       KC_PGDOWN,           KC_END, XXXXXXX,
-      RAISE, XXXXXXX, XXXXXXX, LCTL(LSFT(KC_LEFT)),       LCTL(LSFT(KC_RGHT)),          XXXXXXX, XXXXXXX,                        XXXXXXX, KC_APP,  KC_LEFT,       KC_UP  ,       KC_DOWN,             KC_RGHT, XXXXXXX,
-    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,          XXXXXXX,           XXXXXXX,     XXXXXXX,       			XXXXXXX, LCTL(KC_HOME), XXXXXXX, XXXXXXX, LCTL(KC_END), XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   LOWER,   KC_SPC ,KC_LCTL,     KC_ENT, KC_RSFT ,  KC_BSPC,          RAISE, XXXXXXX, XXXXXXX, XXXXXXX 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,             XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX,          XXXXXXX,       XXXXXXX,       XXXXXXX,             XXXXXXX,      XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, LCTL(KC_LEFT),             LCTL(KC_RGHT),       XXXXXXX, XXXXXXX,                        XXXXXXX, RCTL(KC_SLSH),    KC_HOME,       KC_PGUP,       KC_PGDOWN,           KC_END,       XXXXXXX,
+      RAISE, XXXXXXX, XXXXXXX, LCTL(LSFT(KC_LEFT)),       LCTL(LSFT(KC_RGHT)), XXXXXXX, XXXXXXX,                        XXXXXXX, KC_APP,           KC_LEFT,       KC_UP  ,       KC_DOWN,             KC_RGHT,      XXXXXXX,
+    KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,             XXXXXXX,              XXXXXXX,     XXXXXXX,       XXXXXXX,          LCTL(KC_HOME), XXXXXXX,       XXXXXXX,             LCTL(KC_END), XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   LOWER,               KC_SPC , KC_LCTL,     KC_ENT,      KC_RSFT ,      KC_BSPC,          RAISE, XXXXXXX, XXXXXXX,      XXXXXXX 
   ),
 
   /* Adjust  (Both Raise and Lower Together)
